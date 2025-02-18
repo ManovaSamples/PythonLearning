b498898315d77bf58a83ea43f245b3ee1d3ec617
mano.c.91 <mano.c.91@gmail.com>
2025-02-18 14:43:19 +0000

    Factorial output achieved 	modified:   GenAI/Session_3/Hometask/1.py

--- GenAI/Session_3/Hometask/1.py
+++ GenAI/Session_3/Hometask/1.py
@@ -8,7 +8,7 @@ def printFactorial(num):
     else:
         factorial = 1
         for i in range(1, num+1):
-            factorial = factorial * 1
+            factorial = factorial * i
             # print(factorial)
         return(factorial)
 
