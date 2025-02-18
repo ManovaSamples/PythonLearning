57343c5da0ce30b6eba7c27c01b4fc0fa573fc7c
mano.c.91 <mano.c.91@gmail.com>
2025-02-18 10:31:32 +0000

    append and remove task 	modified:   GenAI/Session_4/ClassWork/classtask.py

--- GenAI/Session_4/ClassWork/classtask.py
+++ GenAI/Session_4/ClassWork/classtask.py
@@ -1,4 +1,4 @@
-numbers = [1,7,2,4,9,3]
+# numbers = [1,7,2,4,9,3]
 
 ##print list elements
 # for i in numbers:
@@ -7,6 +7,18 @@ numbers = [1,7,2,4,9,3]
 #index
 # print(numbers[3])
 
-# iterate through elements using index
-for i in range(len(numbers)):
-    print(numbers[i])
+# # iterate through elements using index
+# for i in range(len(numbers)):
+#     print(numbers[i])
+
+# creat a list contains the numbers 1-10 automatically
+
+numbers2 = []
+
+for i in range(1, 11):
+    numbers2.append(i)
+print(numbers2)
+numbers2.remove(numbers2[-1])
+print(numbers2)
+
+
