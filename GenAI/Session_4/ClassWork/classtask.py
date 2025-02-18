4e548e534d8afef4b8a1e40b25a4d39a8185b438
mano.c.91 <mano.c.91@gmail.com>
2025-02-18 10:24:11 +0000

    session_4 classwork 	new file:   GenAI/Session_4/ClassWork/classtask.py

---
+++ GenAI/Session_4/ClassWork/classtask.py
@@ -0,0 +1,12 @@
+numbers = [1,7,2,4,9,3]
+
+##print list elements
+# for i in numbers:
+#     print(i)
+
+#index
+# print(numbers[3])
+
+# iterate through elements using index
+for i in range(len(numbers)):
+    print(numbers[i])
