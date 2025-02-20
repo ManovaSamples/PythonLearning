26d6ab60c9be02f946a0b39f002ec351d731aa08
mano.c.91 <mano.c.91@gmail.com>
2025-02-20 03:22:08 +0000

    modified:   GenAI/Session_4/Hometask/1.py 	modified:   GenAI/Session_4/Hometask/3.py 	modified:   README.md

--- GenAI/Session_4/Hometask/3.py
+++ GenAI/Session_4/Hometask/3.py
@@ -1,2 +1,2 @@
-#write a cuntion that takes a string and returns a dictionary where the keys are words from 
-#thestring and the values are thier occurances, ignore capitalization and punctuation
+#write a funtion that takes a string and returns a dictionary where the keys are words from 
+#the string and the values are thier occurances, ignore capitalization and punctuation
